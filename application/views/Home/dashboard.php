6401989f39073a24deb1db617c2ee961e3b99a12
bilalazhar18 <32155462+bilalazhar18@users.noreply.github.com>
2017-10-20 18:45:51 +0500

    comit

--- application/views/Home/dashboard.php
+++ application/views/Home/dashboard.php
@@ -13,7 +13,7 @@
             <div class="icon">
               <i class="fa fa-bus" aria-hidden="true"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            <a href="<?php echo base_url()?>Trip/index" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
         <!-- ./col -->
@@ -43,7 +43,7 @@
             <div class="icon">
               <i class="ion ion-person-add"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            <a href="<?php echo base_url()?>Defination/contractorpage" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
         <!-- ./col -->
@@ -58,7 +58,7 @@
             <div class="icon">
               <i class="fa fa-truck" aria-hidden="true"></i></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            <a href="<?php echo base_url()?>Vehicle/show_vehicle" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
         <!-- ./col -->
